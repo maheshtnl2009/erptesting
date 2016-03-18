7f3cf8101b3e97350f2f8252d506bb91923ce3ea
maheshtnl2009 <maheshtnl2009@gmail.com>
2016-03-18 10:53:26 -0400

    erp smoke xml file creeated and updating to git

--- abc.com/src/test/java/com/automationtest/Auttest.java
+++ abc.com/src/test/java/com/automationtest/Auttest.java
@@ -1,9 +1,10 @@
 package com.automationtest;
 import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.ERPAnalysts.pages.GenerateResults;;
+import com.ERPAnalysts.pages.GenerateResults;
 
 public class Auttest {
 
